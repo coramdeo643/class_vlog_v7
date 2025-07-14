d090520819a7eeb854cf84409750b620e724bca6
coramdeo643 <coramdeo643@kakao.com>
2025-07-14 12:52:44 +0900

    250714 RestAPI 컨트롤러 요청과 응답

--- src/main/java/com/tenco/blog/user/UserService.java
+++ src/main/java/com/tenco/blog/user/UserService.java
@@ -3,7 +3,6 @@ package com.tenco.blog.user;
 import com.tenco.blog._core.errors.exception.Exception400;
 import com.tenco.blog._core.errors.exception.Exception404;
 import lombok.RequiredArgsConstructor;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,55 +13,61 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true) // 클래스 레벨에서의 읽기 전용 설정
 public class UserService {
 
-    private static final Logger log = LoggerFactory.getLogger(UserService.class);
-    private final UserJpaRepository userJpaRepository;
+	private static final Logger log = LoggerFactory.getLogger(UserService.class);
+	private final UserJpaRepository userJpaRepository;
 
 
-    /**
-     * 회원가입 처리
-     */
-    @Transactional // 메서드 레벨에서 쓰기 전용 트랜잭션 활성화
-    public User join(UserRequest.JoinDTO joinDTO) {
-        //1. 사용자명 중복 체크
-        userJpaRepository.findByUsername(joinDTO.getUsername())
-                .ifPresent(user1 -> {
-                    throw new Exception400("이미 존재하는 사용자명입니다");
-                });
-        return userJpaRepository.save(joinDTO.toEntity());
-    }
+	/**
+	 * 회원가입 처리 - DTO 변환 책임
+	 */
+	@Transactional // 메서드 레벨에서 쓰기 전용 트랜잭션 활성화
+	public UserResponse.JoinDTO join(UserRequest.JoinDTO joinDTO) {
+		//1. 사용자명 중복 체크
+		userJpaRepository.findByUsername(joinDTO.getUsername())
+				.ifPresent(user1 -> {
+					throw new Exception400("이미 존재하는 사용자명입니다");
+				});
+		// 2. 회원가입 처리
+		User savedUser = userJpaRepository.save(joinDTO.toEntity());
+		// 3. 응답 DTO로 변환해서 반환 일을 시킨다
+		return new UserResponse.JoinDTO(savedUser);
+	}
 
-    /**
-     * 로그인 처리
-     */
-    public User login(UserRequest.LoginDTO loginDTO) {
-        return userJpaRepository
-                .findByUsernameAndPassword(loginDTO.getUsername(), loginDTO.getPassword())
-                .orElseThrow(() -> {
-                    return new Exception400("사용자명 또는 비밀번호가 틀렸어요");
-                });
-    }
+	/**
+	 * 로그인 처리 - DTO 변환 책임
+	 */
+	public UserResponse.LoginDTO login(UserRequest.LoginDTO loginDTO) {
+		// 회원 정보 조회
+		User selectedUser = userJpaRepository
+				.findByUsernameAndPassword(loginDTO.getUsername(), loginDTO.getPassword())
+				.orElseThrow(() -> new Exception400("사용자명 또는 비밀번호가 틀렸어요"));
 
-    /**
-     *  사용자 정보 조회
-     */
-    public User findById(Long id) {
-        return userJpaRepository.findById(id).orElseThrow(() -> {
-            log.warn("사용자 조회 실패 - ID {}", id);
-            return new Exception404("사용자를 찾을 수 없습니다");
-        });
-    }
+		// User Entity를 응답 DTO 변환해서 반환처리
+		return new UserResponse.LoginDTO(selectedUser);
+	}
 
-    /**
-     *  회원정보 수정 처리 (더티 체킹)
-     */
-    @Transactional
-    public User updateById(Long userId, UserRequest.UpdateDTO updateDTO) {
-        // 1.
-        // 2. 사용자 조회
-        // 3. 수정된 User 반환 왜? --> 세션 동기화 때문!!!
-        User user = findById(userId);
-        // user.update(updateDTO);  TODO 추후 추가
-        user.setPassword(updateDTO.getPassword());
-        return user;
-    }
+	/**
+	 * 사용자 정보 조회 - 응답 DTO 반환 책임
+	 */
+	public UserResponse.DetailDTO findById(Long id) {
+
+		// 인증처리 > 세션기반
+		// 권한 처리 일단 생략
+		User selectedUser = userJpaRepository.findById(id).orElseThrow(() -> {
+			log.warn("사용자 조회 실패 - ID {}", id);
+			return new Exception404("사용자를 찾을 수 없습니다");
+		});
+		// 응답 DTO 변환 처리
+		return new UserResponse.DetailDTO(selectedUser);
+	}
+
+	/**
+	 * 회원정보 수정 처리 (더티 체킹)
+	 */
+	@Transactional
+	public UserResponse.UpdateDTO updateById(Long userId, UserRequest.UpdateDTO updateDTO) {
+		User updatedUser = new UserResponse.UpdateDTO(userId);
+		updatedUser.setPassword(updateDTO.getPassword());
+		return new UserResponse.UpdateDTO(updatedUser);
+	}
 }
